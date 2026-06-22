fc987cf6363acde66686ce5b5335122d9c469f7a
huyuanchao <huyuanchao@boohee.com>
2026-06-22 09:32:02 +0800

    通过向上查找 go.mod 动态定位项目根目录，替代硬编码的 app 路径检测逻辑。

--- tool/kratos-protoc/grpc.go
+++ tool/kratos-protoc/grpc.go
@@ -29,6 +29,21 @@ func installGRPCGen() error {
 	return nil
 }
 
+// findProjectRoot 从 start 目录开始向上查找，直到找到包含 go.mod 的目录
+func findProjectRoot(start string) (string, error) {
+	dir := start
+	for {
+		if _, err := os.Stat(filepath.Join(dir, "go.mod")); err == nil {
+			return dir, nil
+		}
+		parent := filepath.Dir(dir)
+		if parent == dir {
+			return "", fmt.Errorf("go.mod not found starting from %s", start)
+		}
+		dir = parent
+	}
+}
+
 func genGRPC(files []string) error {
 	pwd, _ := os.Getwd()
 	gosrc := path.Join(gopath(), "src")
@@ -37,19 +52,33 @@ func genGRPC(files []string) error {
 		return err
 	}
 
-	i := strings.Index(pwd, "app")
-	cmdDir := filepath.Dir(pwd[:i-1])
-	var cmdFiles []string
-	for _ ,file := range files {
-		cmdFiles = append(cmdFiles, fmt.Sprintf("%s/%s", pwd[len(cmdDir)+1:], file))
+	// 以 go.mod 所在目录作为项目根
+	projectRoot, err := findProjectRoot(pwd)
+	if err != nil {
+		return err
 	}
 
-	line := fmt.Sprintf(_grpcProtoc, gosrc, ext, pwd)
+	// 项目名和项目根的父目录
+	projectName := filepath.Base(projectRoot)
+	parentOfProject := filepath.Dir(projectRoot)
+
+	// proto 文件相对于项目根的路径
+	relPwd, err := filepath.Rel(projectRoot, pwd)
+	if err != nil {
+		return err
+	}
+	// 让 RegisterFile 带上项目名前缀
+	var cmdFiles []string
+	for _, file := range files {
+		cmdFiles = append(cmdFiles, filepath.Join(projectName, relPwd, file))
+	}
+
+	line := fmt.Sprintf(_grpcProtoc, gosrc, ext, parentOfProject)
 	log.Println(line, strings.Join(cmdFiles, " "))
 	args := strings.Split(line, " ")
 	args = append(args, cmdFiles...)
 	cmd := exec.Command(args[0], args[1:]...)
-	cmd.Dir = cmdDir
+	cmd.Dir = parentOfProject
 	cmd.Env = os.Environ()
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
